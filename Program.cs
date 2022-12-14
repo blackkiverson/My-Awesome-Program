571161d24c5dafa4bc9d63785c06a78b485ccc4b
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-12-14 23:32:07 +0100

    Learning to Declare Variables

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿namespace My_Awesome_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+            
+            // Declaring a Variable
+            string name = "John";
+            Console.WriteLine("My nme is" + " " + name);
+
+            /* Waiting for the user to press a key before closing the console window. */
+            Console.ReadKey();
+        }
+    }
+}
