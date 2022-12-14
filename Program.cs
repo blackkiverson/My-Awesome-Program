962b6511d8d2df28cdf7e9963fbcb232fada5741
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-12-14 23:46:35 +0100

    ...

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@
             // Declaring a Variable
             string name = "John";
             Console.WriteLine("My nme is" + " " + name);
+            // Double for decimal point numbers
+            double myDoubleNum = 67.9D;
+            myDoubleNum = 09028570136D;
+            Console.WriteLine(myDoubleNum);
+            // Int for whole numbers
+            int myNum = 5;
+            Console.WriteLine(myNum);
+            // Char for Single characters
+            char myLetter = 'D';
+            Console.WriteLine(myLetter);
+            // Bool for true or false value
+            bool myBool = true;
+            Console.WriteLine(myBool);
 
             /* Waiting for the user to press a key before closing the console window. */
             Console.ReadKey();
